51c396321f4a73a0f692ba413b6c9cab7d89aa97
baytur10 <bilalidin0wsss01@gmail.com>
2024-12-03 16:51:28 +0300

    Add files via upload

---
+++ dz1/main.js
@@ -0,0 +1,25 @@
+let num1 = prompt('Введите число:')
+if (num1 % 2 === 0 ) {
+    console.log('Четное число');
+    
+    
+} else {
+    console.log('Нечетное число');
+    
+}
+
+
+
+let num = prompt('Введите число:')
+alert( num )
+if (num < 0 ) {
+    console.log('Отрицательное число');
+    
+    
+} else if ( num > 0) {
+    console.log('Положительное число');
+    
+} else {
+    console.log( 'Чисо равно нулю');
+    
+}
